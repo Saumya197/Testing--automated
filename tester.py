bdbd24137144ceb037803674165bea1e8fe0a2c3
Saumya <t.saumya@iitg.in>
2023-05-22 00:09:46 +0530

    commmiiiiiiit

--- tester.py
+++ tester.py
@@ -1,8 +1,9 @@
 from subprocess import Popen
-from os import listdir
+from os import listdir,system
 from time import perf_counter as pcr
-file=listdir(".")[0]
-print(file)
+# file=listdir(".")[0]
+# print(file)
 # st=pcr()
 # Popen(["python",f"payload/{file}"])
 # print(pcr()-st)
+os.system("python falana.py")
