c3cd4881e3e64ebc69fc9a48ec39ec53b4c7609b
Saumya <t.saumya@iitg.in>
2023-05-21 23:59:03 +0530

    commmiit

--- tester.py
+++ tester.py
@@ -1,7 +1,7 @@
 from subprocess import Popen
 from os import listdir
 from time import perf_counter as pcr
-file=listdir("payload")[0]
+file=listdir("./payload")[0]
 st=pcr()
 Popen(["python",f"payload/{file}"])
 print(pcr()-st)
